6bd6a466c090b40c75f2a4b07a50f647ccce804d
Andrew Omondi <andrew.omondi@microsoft.com>
2020-02-29 08:50:33 +0300

    Add project files.

---
+++ MSALDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MSALDemo
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string ClientId = "";
+            string Tenant = "";
+            string[] scopes = new string[] { "User.Read"};
+
+            // Create an application to call apis
+            var publicClientApplication = PublicClientApplicationBuilder.Create(ClientId)
+                .WithRedirectUri("http://localhost:1234")
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .Build();
+
+            // get the token
+            var authResult = await publicClientApplication.AcquireTokenInteractive(scopes)
+                .ExecuteAsync();
+            var token = authResult.AccessToken;
+
+            // Try sending a message across with the Auth header set
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", token);
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me/");
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            // Print out response
+            var response = await httpResponseMessage.Content.ReadAsStringAsync();
+            string jsonFormatted = JValue.Parse(response).ToString(Formatting.Indented);
+            Console.WriteLine(jsonFormatted);
+        }
+    }
+}
